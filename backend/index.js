e3d6c040a23e50c073680dd2558dc7f478ff776e
Wesner M <wesnerm@gmail.com>
2023-07-26 14:07:21 -0400

    TestingchatNode

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": "166ad0f7-dc4f-4838-a402-f53b8bdcc405 " } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3001);
